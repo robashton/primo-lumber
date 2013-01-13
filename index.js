fd8217b340839bc8e49350d448c4bdcaffdd18d9
Rob Ashton <robashton@codeofrob.com>
2013-01-13 01:49:42 +0000

    It works

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 var Glob = require('glob').Glob
+var fs = require('fs')
+var path = require('path')
 var argv = require('optimist')
     .usage('Convert a pile of matching files to base64.\n Usage: $0')
     .demand('p')
     .alias('p', 'pattern')
-    .describe('f', 'The pattern to match when converting files')
+    .describe('p', 'The pattern to match when converting files')
+    .demand('d')
+    .alias('d', 'directory')
+    .describe('d', 'The directory we\'re working in')
     .argv
 
+var pattern = argv.p
+  , directory = argv.d
 
-var glob = new Glob(argv.p)
+var glob = new Glob(pattern,{ cwd: directory})
+
 glob.on('match', function(file) {
-  console.log(file)
+  if(file.indexOf('.base64') >= 0) return
+  file = path.join(directory, file)
+  fs.readFile(file, 'base64', function(err, data) {
+    if(err) return handleError(err)
+    console.log('Processing', file)
+    fs.writeFile(file + '.base64', data, 'utf8')
+  })
 })
+glob.on('end', function() {
+
+})
+glob.on('error', handleError)
 
 
 
 
+function handleError(err) { 
+  console.error(err)
+}
 
